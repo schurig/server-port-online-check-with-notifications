8a1adf4449cac755c1d2421ad0ae42d3efbd7f4c
Martin Schurig <schurig@users.noreply.github.com>
2015-04-24 17:50:41 +0200

    cleanup

--- server-port-online-check-with-notifications/server_online_check.php
+++ server-port-online-check-with-notifications/server_online_check.php
@@ -18,13 +18,10 @@ $ports   = array(80,443,22,25,993,995); // enter all ports (services) you want y
 
 
 class StatusCheck {
-    
     // Notification Settings
-    
     private $alert_email     = 'ENTER_EMail_Here';
     private $pushover_token  = 'ENTER_Pushover_Token';
     private $pushover_user   = 'ENTER_Pushover_User_Key';
-    
 
     private $server         = false;
     private $ports          = false;
@@ -57,9 +54,7 @@ class StatusCheck {
      */
 
     public function StatusCheck($server = false, $ports = false) {
-
         if(!$server) {
-
             echo 'wrong input!';
         }
 
@@ -67,12 +62,10 @@ class StatusCheck {
         $this->ports    =   $ports;
 
         if(!$this->checkIP()) {
-
             return $this->notification(array('title' => 'Server Alert', 'message' => $this->server . ': Server down'));
         }
 
         $this->checkPorts();
-
         return true;
     }
     
@@ -83,23 +76,17 @@ class StatusCheck {
      */
 
     public function checkIP($step = 1) {  
-
         $pingresult = exec('/bin/ping -c 1 ' . $this->server, $outcome, $status);
-
         if ($status == 0) {
-
             return true;
-
         }
 
         if($step == 1) {
-
             sleep(3);
             return $this->checkIP($step++);
         }
 
         return false;
-
     }
     
     /**
@@ -108,35 +95,25 @@ class StatusCheck {
      */
 
     public function checkPorts() {
-
         foreach($this->ports as $port) {
-
             $service = $this->services[$port];
-
             $res = $this->checkPort($this->server, $port, $service);
-
             if(!$res) {
-
                 $this->notification(array('title' => 'Server Alert', 'message' => $this->server . ': The service ' . $service . ' (' . $port . ') does not respond'));
             }
         }
-
+        
         return true;
     }
 
     public function checkPort($server, $port = 80, $firstTime = true) {
-
         if(!is_int($port) || empty($server)) {
-
             return false;
         }
 
         $check = @fsockopen($server, $port, $errno, $errstr, 2);
-
             if(!$check) {
-
                 if($firstTime) {
-
                     sleep(3);
                     return $this->checkPort($server, $port, false);
                 }
@@ -154,16 +131,13 @@ class StatusCheck {
      */
 
     private function notification($notification = false) {
-
         if(!$notification) {
-
             return 'notification error';
         }
 
         $m_title   = $notification['title'];
         $m_message = $notification['message'];
 
-        
         curl_setopt_array($ch = curl_init(), array(
           CURLOPT_URL => "https://api.pushover.net/1/messages.json",
           CURLOPT_RETURNTRANSFER => 1,
@@ -177,14 +151,11 @@ class StatusCheck {
         $res = curl_exec($ch);
         curl_close($ch);
         
-
         mail($this->alert_email, $m_title, $m_message);
-
     }
 }
 
 $status = new StatusCheck($server, $ports);
-
 // you like it? send me a cookie :)
 // you don't? :( ..send me a cookie anyway *___*
 ?>
